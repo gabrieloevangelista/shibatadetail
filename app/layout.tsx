195278dd0678fc70fccdbce6d57389efc94669ed
Gabriel <gabriel.evan.queiroz@gmail.com>
2025-09-18 11:03:45 -0300

    feat(analytics): add Google Tag Manager integration
    Implement GTM tracking for website analytics by adding both script and noscript tags to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,6 +81,15 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KVPWC4XV');`
+        }} />
+        {/* End Google Tag Manager */}
         <link rel="icon" href="/favicon.svg" sizes="any" />
         <link rel="icon" href="/favicon.png" type="image/png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
@@ -93,6 +102,16 @@ html {
         `}</style>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KVPWC4XV"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SkipNavigation />
         {children}
         <WhatsAppFloat />
