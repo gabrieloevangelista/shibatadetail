900d0b0fc6e6263e867780ad21d06d10d7dfe9f7
Gabriel <gabriel.evan.queiroz@gmail.com>
2025-09-18 00:37:07 -0300

    style(social-proof): increase default logo size from w-8 to w-9

--- components/social-proof.tsx
+++ components/social-proof.tsx
@@ -5,55 +5,55 @@ export function SocialProof() {
       <div className="self-stretch flex flex-nowrap justify-center items-center gap-3 sm:gap-6 md:gap-12 px-4">
         {/* Porsche */}
         <div className="flex items-center justify-center">
-          <div className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center">
+          <div className="w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center">
             <img
               src="/marcas de carros/porsche.png"
               alt="Porsche - Marca atendida pela Shibata Premium Detail"
-              className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 object-contain filter brightness-0 invert"
+              className="w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 object-contain filter brightness-0 invert"
             />
           </div>
         </div>
         
         {/* BMW */}
         <div className="flex items-center justify-center">
-          <div className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center">
+          <div className="w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center">
             <img
               src="/marcas de carros/bmw.png"
               alt="BMW - Marca atendida pela Shibata Premium Detail"
-              className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 object-contain filter brightness-0 invert"
+              className="w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 object-contain filter brightness-0 invert"
             />
           </div>
         </div>
         
         {/* Mercedes-Benz */}
         <div className="flex items-center justify-center">
-          <div className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center">
+          <div className="w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center">
             <img
               src="/marcas de carros/mercedes.png"
               alt="Mercedes-Benz - Marca atendida pela Shibata Premium Detail"
-              className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 object-contain filter brightness-0 invert"
+              className="w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 object-contain filter brightness-0 invert"
             />
           </div>
         </div>
         
         {/* Audi */}
         <div className="flex items-center justify-center">
-          <div className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center">
+          <div className="w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center">
             <img
               src="/marcas de carros/audi.png"
               alt="Audi - Marca atendida pela Shibata Premium Detail"
-              className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 object-contain filter brightness-0 invert"
+              className="w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 object-contain filter brightness-0 invert"
             />
           </div>
         </div>
         
         {/* Volvo */}
         <div className="flex items-center justify-center">
-          <div className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center">
+          <div className="w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center">
             <img
               src="/marcas de carros/volvo.png"
               alt="Volvo - Marca atendida pela Shibata Premium Detail"
-              className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 object-contain filter brightness-0 invert"
+              className="w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 object-contain filter brightness-0 invert"
             />
           </div>
         </div>
